c38c510598709bb92663f09a3bf7df5f94af26fa
Rohit Sriram <rohitsriram14@gmail.com>
2019-01-18 12:32:02 -0800

    Initial Commit

---
+++ FlixsterTests/FlixsterTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlixsterTests.swift
+//  FlixsterTests
+//
+//  Created by rohit sriram on 1/18/19.
+//  Copyright © 2019 rohitsriram. All rights reserved.
+//
+
+import XCTest
+@testable import Flixster
+
+class FlixsterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
